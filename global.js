ec26ee8314f3e8d948e782846a3223d0c6804e71
LIMDONGJUN <dlaehd1994@naver.com>
2020-07-01 11:03:03 +0900

    API주소 수정

--- global.js
+++ global.js
@@ -1,2 +1,4 @@
-global.API_URI="http://54.180.167.12:5000"
+//global.API_URI="http://54.180.167.12:5000"
+global.API_URI="http://13.125.80.35:5000"
+
 global.token=""
--- src/UpperMenu.js
+++ src/UpperMenu.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import {ScrollView ,TouchableOpacity} from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native';
 import { Container, Tab, Tabs, ScrollableTab, Text, Button, Spinner } from 'native-base';
 import PostCard from './PostCard'
 import HomeTab from './HomeTab'
 import SearchTab from './SearchTab'
 import axios from 'axios'
 import {
-      View,
-      Image,
-      FlatList, // here
-  } from 'react-native';
+    View,
+    Image,
+    FlatList, // here
+} from 'react-native';
 
 /*import { createMaterialTopTabNavigator, createAppContainer } from 'react-navigation'; 
 
@@ -29,29 +29,29 @@ export default class UpperMenu extends React.Component({navigation}){
 export default class UpperMenu extends React.Component {
 
     state = {
-      data: [],
-      page: 1,
-      isLoading : true,
+        data: [],
+        page: 1,
+        isLoading: true,
     }
-  
-    sendPost = async ()=>{
-        try{
-            const data_ = await axios({ 
-                url: global.API_URI+"/api/post",
-                method:'get',
-                headers : {
+
+    sendPost = async () => {
+        try {
+            const data_ = await axios({
+                url: global.API_URI + "/api/post",
+                method: 'get',
+                headers: {
                     'x-access-token': global.token
                 }
             })
             //console.log(data_.body)
             //console.log(data_.data) // data에만 들어있었다
-            this.setState({ 
+            this.setState({
                 data: this.state.data.concat(data_.data),
                 page: this.state.page + 1,
-                isLoading : false
-              });
-            
-        }catch(err){
+                isLoading: false
+            });
+
+        } catch (err) {
             console.log(err);
         }
     }
@@ -61,36 +61,38 @@ export default class UpperMenu extends React.Component {
     }
 
     componentDidMount() {
-      this.sendPost();
+        this.sendPost();
     }
 
 
-  
-    _renderItem = ({item,index}) => (
-     <TouchableOpacity key={index} ><PostCard key={index} title = {item.title} content = {item.content} author ={item.author.userId}/></TouchableOpacity>
-      /*<View style={{borderBottomWidth:1, marginTop: 20, height:200}}>
-        <Text>{item.title}</Text>
-      </View>*/
-      //onPress={()=>navigation.navigate("Details",{pageId : item})}
+
+    _renderItem = ({ item, index }) => (
+        <TouchableOpacity key={index} onPress={()=>this.props.navigation.navigate('Details')}>
+            <PostCard key={index} title={item.title} content={item.content} author={item.author.userId} />
+        </TouchableOpacity>
+        /*<View style={{borderBottomWidth:1, marginTop: 20, height:200}}>
+          <Text>{item.title}</Text>
+        </View>*/
+        //onPress={()=>navigation.navigate("Details",{pageId : item})}
     );
-  
+
     render() {
-        const {isLoading} = this.state;
-        if(isLoading){
-            return <View style ={{height:"100%", alignItems:"center", justifyContent:"center"}}><Spinner/></View>
+        const { isLoading } = this.state;
+        if (isLoading) {
+            return <View style={{ height: "100%", alignItems: "center", justifyContent: "center" }}><Spinner /></View>
         }
         return (
-            
-          <FlatList 
-            data={this.state.data}
-            renderItem={this._renderItem}
-            keyExtractor={(item, index) => item._id}
+
+            <FlatList
+                data={this.state.data}
+                renderItem={this._renderItem}
+                keyExtractor={(item, index) => item._id}
             //onEndReached={this._handleLoadMore}
             //onEndReachedThreshold={0.1}
-          />
+            />
         );
-      }
-  }
+    }
+}
 
 /*let Anot = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 export default function UpperMenu({navigation}) {
